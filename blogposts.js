d8df2ae17a53cbb686eaaceb26be1da50068ac18
KavyanshKhaitan2 <73186427+KavyanshKhaitan2@users.noreply.github.com>
2021-06-21 21:42:20 +0530

    Add files via upload

--- blogposts.js
+++ blogposts.js
@@ -2,11 +2,15 @@
 var blogposts = [
   {
     title: "Example Post 1",
-    blog_content: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?`
+    blog_content: `
+    Type your post here like what you did in your videos or other things and this can contain newlines
+    Like
+    This
+    `
   },
   
   {
     title: "Example Post 2",
-    blog_content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+    blog_content: `Type your post here like what you did in your videos or other things`
   },
 ]
